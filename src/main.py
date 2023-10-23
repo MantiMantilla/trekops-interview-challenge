5373a2d6985ed5b204f709776f93fb62335e5b8b
Alejandro Mantilla <alejomantilla@protonmail.com>
2023-10-23 02:11:14 -0500

    First commit, in preparation for the interview challenge.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# # Trekops Data Analytics Challenge
+#
+# This notebook is my take on the Trekops Data Analytics Challenge for the Data Analyst job application.
+
+import pandas
+import seaborn as sns
+import plotly
+
+# A Markdown cell!!
+
+print("A Python Cell!")
